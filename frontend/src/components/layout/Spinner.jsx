0ecd8bac288d0cd30f0b73a8bafebd8bda0a30a2
Harshil Kachhadiya <harshil23kachhadiya@gmail.com>
2025-08-31 10:39:13 +0530

    changes some files

--- frontend/src/components/layout/Spinner.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Spinner = () => (
-  <div className="flex items-center justify-center min-h-[100px]">
-    <div className="w-10 h-10 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"></div>
-  </div>
-);
-
-export default Spinner;
--- frontend/src/utils/ErrorMessage.js
+++
@@ -1,7 +0,0 @@
-export function getErrorMessage(error) {
-  const mess =
-    error.response?.data?.message ||
-    error.message ||
-    "An unexpected error occurred";
-  return mess;
-}
