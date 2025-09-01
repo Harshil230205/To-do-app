3e16a9137ad252870f14fda22a0ae2a5ca973c02
Harshil Kachhadiya <harshil23kachhadiya@gmail.com>
2025-09-01 21:11:42 +0530

    add some changes

--- backend/frontend/src/App.jsx
+++ backend/frontend/src/App.jsx
@@ -1,12 +1,8 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
-import TodoList from "./pages/TodoList";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
-import Contact from "./pages/Contact";
 import NoPage from "./pages/NoPage";
-import Layout from "./pages/Layout";
 import { Toaster } from "react-hot-toast";
 
 function App() {
@@ -14,10 +10,6 @@ function App() {
     <div className="dark min-h-screen bg-gray-900 text-gray-100">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/layout" element={<Layout />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/todos" element={<TodoList />} />
-        <Route path="/contact" element={<Contact />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<NoPage />} />
